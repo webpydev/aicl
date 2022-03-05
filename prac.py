4573536d4887539e479b9c2b0ff3fec2dcaf9ba6
webpydev <100967110+webpydev@users.noreply.github.com>
2022-03-05 12:27:03 +0530

    assignment 1
    assignment 1

---
+++ prac.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class EATABLES(BaseModel):
+    favourite: str
+    rate: int
+    confess: bool
+    others: Optional[list]
+
+@app.get("/groceries/{input}")
+
+def kilo(input, w:Optional[str]=None):
+    return{"fruits":"veggies","type":input,"display":w}
+
+@app.put("/chocolates")    
+
+async def other(fries:EATABLES):
+    return{"all time favourite":fries.favourite}
+
+
+
